c5120193df947ec4a5e75f32506ea209b9f5c18b
Sam <89226291+Sam-or-something@users.noreply.github.com>
2022-06-06 13:05:44 -0300

    Initial commit

---
+++ Assets/Scripts/ControladorJugador.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControladorJugador : MonoBehaviour
+{
+    public float movementSpeed, jumpSpeed, onFloor;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(0, 0, movementSpeed);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(0, 0, -movementSpeed);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(movementSpeed, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(-movementSpeed, 0, 0);
+        }
+        if(Input.GetKey(KeyCode.Space) && onFloor < 2){
+            transform.Translate(0, jumpSpeed, 0);
+            onFloor += 1;
+        }
+    }
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "Piso")
+        {
+            onFloor = 0;
+        }
+    }
+}
